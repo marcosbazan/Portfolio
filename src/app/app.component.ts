e6a68617d13c95003549e1ccb1cc32d09c255e31
marcosbazan2002@gmail.com <marcosbazanf@hotmail.com>
2025-07-30 12:28:35 +0200

    modificando perfil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfilesComponent } from "./Pages/Profiles/profiles.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfilesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
