901ddc59ebaf126d77dbe0589966d9b1a852e804
marcosbazan2002@gmail.com <marcosbazanf@hotmail.com>
2025-08-05 19:50:27 +0200

    perfil terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfilesComponent } from "./Pages/Profiles/profiles.component";
 import { MenuComponent } from './Pages/Home/Menu/menu.component';
+import { SkillsComponent } from "./Pages/Skills/skills.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfilesComponent, MenuComponent],
+  imports: [RouterOutlet, ProfilesComponent, MenuComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
