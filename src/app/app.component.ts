6179ad0cb2fbda52146b52f0273d66f773d2ebb8
marcosbazan2002@gmail.com <marcosbazanf@hotmail.com>
2025-08-11 11:56:23 +0200

    creando estudios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { ProfilesComponent } from "./Pages/Profiles/profiles.component";
 import { MenuComponent } from './Pages/Menu/menu.component';
 import { SkillsComponent } from "./Pages/Skills/skills.component";
 import { ExpeciencieComponent } from './Pages/Expeciencie/expeciencie.component';
+import { LearningComponent } from './Pages/Learning/learning.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfilesComponent, MenuComponent, SkillsComponent,ExpeciencieComponent],
+  imports: [RouterOutlet, ProfilesComponent, MenuComponent, SkillsComponent, ExpeciencieComponent, LearningComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
