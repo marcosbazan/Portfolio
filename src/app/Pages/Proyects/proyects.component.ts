1ae25abe981b3022af27c794109120513ebf66b1
marcosbazan2002@gmail.com <marcosbazanf@hotmail.com>
2025-09-23 12:46:39 +0200

    añadiendo proyectos

--- src/app/Pages/Proyects/proyects.component.ts
+++ src/app/Pages/Proyects/proyects.component.ts
@@ -16,19 +16,22 @@ export class ProyectsComponent {
   {
     key: 0,
     link: "https://github.com/marcos318888/Portafolio",
+    img: "assets/proyectos/Portfolio.png",
     techs: [
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg",
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
     ]
   },
   {
     key: 1,
-    link: "https://github.com/marcos318888/GaleriaArte",
+    link: "https://github.com/marcos318888/Gestion-Biblioteca",
     techs: [
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original.svg"
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg"
     ]
   },
   {
@@ -36,15 +39,17 @@ export class ProyectsComponent {
     link: "https://github.com/marcos318888/NOMINATOR",
     techs: [
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg"
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqlite/sqlite-original.svg"
     ]
   },
   {
     key: 3,
     link: "https://github.com/marcos318888/Chatbot",
-    techs: [
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jupyter/jupyter-original-wordmark.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"]
+    img: "assets/proyectos/Chatbot.jpg",
+    techs: ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jupyter/jupyter-original-wordmark.svg"
+      ]
     }]
 
   constructor(private translate: TranslateService) {}
@@ -56,4 +61,15 @@ export class ProyectsComponent {
   changeLang(language: string) {
     this.translate.use(language);
   }
+
+  selectedImage: string | null = null;
+
+  openImage(img: string) {
+    this.selectedImage = img;
+  }
+
+  closeImage() {
+    this.selectedImage = null;
+  }
+
 }
