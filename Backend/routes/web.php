d03b7bc80561eee2dd80bfb6c63659e1a11f96b2
Marcos Bazan <marcos@gmail.com>
2026-05-16 20:25:34 +0200

    fix the rutes

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -3,10 +3,4 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-
-if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit(0);
-}
+require __DIR__.'/groups.php';
